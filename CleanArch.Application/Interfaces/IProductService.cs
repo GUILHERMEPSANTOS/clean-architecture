3b79e28df5766fe3f910423b7622153fddcf3e81
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2022-07-24 13:44:41 -0300

    config

--- CleanArch.Application/Interfaces/IProductService.cs
+++ CleanArch.Application/Interfaces/IProductService.cs
@@ -11,8 +11,8 @@ namespace CleanArch.Application.Interfaces
         Task<IEnumerable<ProductDTO>> GetProducts(); 
         Task<ProductDTO> GetById(int? id);
         // Task<ProductDTO> GetProductCategory(int? id);
-        // Task Add(ProductDTO productDTO);
-        // Task Update(ProductDTO productDTO);
-        // Task Remove(int? id);
+        Task Add(ProductDTO productDTO);
+        Task Update(ProductDTO productDTO);
+        Task Remove(int? id);
     }
 }
